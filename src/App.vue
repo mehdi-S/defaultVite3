0aa599f23b5cc4cde648477854f64731f910e89e
mehdi-S <silini.mehdi@outlook.fr>
2023-11-04 20:27:25 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Home from './components/ThunderHome.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <Home />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ThunderHome.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Yes</h1>
+</template>
